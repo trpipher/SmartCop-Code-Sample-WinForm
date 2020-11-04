7128dc3da16be13ebed66401cf07bffe508e2e36
Thomas Pipher <trpipher@gmail.com>
2020-11-04 15:22:13 -0600

    Added Soda Class

---
+++ Soda.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCop_Code_Sample_WinForm
+{
+    class Soda : Drink
+    {
+        public override string Description
+        {
+            get
+            {
+                return _name + (_carbonated ? ", carbonated." : ", not carbonated.");
+            }
+        }
+
+        public Soda(string Name, bool Carbonated) : base(Name, Carbonated)
+        { }
+
+    }
+}
