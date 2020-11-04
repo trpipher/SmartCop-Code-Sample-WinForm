d4108fcfa401374ee2909e604d9b4cce227b384b
Thomas Pipher <trpipher@gmail.com>
2020-11-04 15:21:32 -0600

    Added Juice Class

---
+++ Juice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCop_Code_Sample_WinForm
+{
+    class Juice : Drink
+    {
+        protected string _fruit;
+        public override string Description
+        {
+            get
+            {
+                return _name + (_carbonated ? ", carbonated" : ", not carbonated") + $", Made of {_fruit}.";
+            }
+        }
+        public Juice(string Name, bool Carb, string Fruit) : base(Name, Carb)
+        {
+            _fruit = Fruit;
+        }
+    }
+}
