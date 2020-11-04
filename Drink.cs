c426053cfba130fdc4289c9eb96fbd8aff93f9b2
Thomas Pipher <trpipher@gmail.com>
2020-11-04 15:20:17 -0600

    Added Drink Class

---
+++ Drink.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCop_Code_Sample_WinForm
+{
+    class Drink
+    {
+        protected string _name;
+        protected bool _carbonated;
+        public virtual string Description
+        {
+            get
+            {
+                return _name + " " + _carbonated;
+            }
+        }
+        public Drink(string Name, bool Carbonated)
+        {
+            _name = Name;
+            _carbonated = Carbonated;
+        }
+    }
+}
