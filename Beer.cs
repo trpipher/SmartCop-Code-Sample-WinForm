aeeb595fbda9f9b81e261ddd7b9c9e2ac6bcfd8a
Thomas Pipher <trpipher@gmail.com>
2020-11-04 15:21:03 -0600

    Added Beer Class

---
+++ Beer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCop_Code_Sample_WinForm
+{
+    class Beer : Drink
+    {
+        protected int _alcohol;
+
+        public override string Description
+        {
+            get
+            {
+                return _name + (_carbonated ? ", carbonated" : ", not carbonated") + $", {_alcohol}%.";
+            }
+        }
+        public Beer(string Name, bool Carbonated, int alcohol) : base(Name, Carbonated)
+        {
+            _alcohol = alcohol;
+        }
+    }
+}
